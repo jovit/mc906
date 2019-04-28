6c6b0ea2dcdfec6c9b93769a8fe3ac298a64d49a
João Vitor Gonçalves <joao_vitor_araki_goncalves@hotmail.com>
2019-04-28 16:08:06 -0300

    Updates

---
+++ precache-manifest.fcbe28765c91c23646c72dd0ea2d5c2f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c52973b98da0f69ad8cd35c8062db9aa",
+    "url": "/mc906/index.html"
+  },
+  {
+    "revision": "dddbdb291843af579cdc",
+    "url": "/mc906/static/css/main.584f321a.chunk.css"
+  },
+  {
+    "revision": "2b0b202d5a028eaa555a",
+    "url": "/mc906/static/js/2.a8d84677.chunk.js"
+  },
+  {
+    "revision": "dddbdb291843af579cdc",
+    "url": "/mc906/static/js/main.9005dd06.chunk.js"
+  },
+  {
+    "revision": "574d8a5e7e7c7fa8b4ef",
+    "url": "/mc906/static/js/runtime~main.bfa16a98.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/mc906/static/media/logo.5d5d9eef.svg"
+  }
+]);
