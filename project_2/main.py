ac74bc1548cadade8e883ecffc5c83dd0801c448
João Vitor Gonçalves <joao_vitor_araki_goncalves@hotmail.com>
2019-04-29 19:36:30 -0300

    face autoencoder

--- project_2/main.py
+++ project_2/main.py
@@ -6,14 +6,16 @@ import tensorflow as tf
 import numpy as np
 from matplotlib import pyplot as plt
 from skimage import io
+from read_data import read_data
 
 mnist = tf.keras.datasets.mnist
 
-(x_train, y_train),(x_test, y_test) = mnist.load_data()
-x_train, x_test = x_train / 255.0, x_test / 255.0
+train = read_data('imgs/train/').astype(float)
+train = train / 255
 
-x_train = np.array([it.flatten() for it in x_train])
-x_test = np.array([it.flatten() for it in x_test])
+train = np.array([it.flatten() for it in train])
+print(len(train[0]))
+#x_test = np.array([it.flatten() for it in x_test])
 
 
 # io.imshow((x_train[0] * 255).reshape((28,28)).astype(int), cmap='gray')
@@ -22,31 +24,31 @@ x_test = np.array([it.flatten() for it in x_test])
 
 
 model = tf.keras.models.Sequential([
-  tf.keras.layers.Dense(28*28),
+  tf.keras.layers.Dense(50*50),
 
   tf.keras.layers.Dense(250, activation=tf.nn.relu),
   tf.keras.layers.Dropout(0.2),
 
-  tf.keras.layers.Dense(50,  activation=tf.nn.sigmoid),
+  tf.keras.layers.Dense(100,  activation=tf.nn.sigmoid),
 
   tf.keras.layers.Dense(250, activation=tf.nn.relu),
   tf.keras.layers.Dropout(0.2),
 
-  tf.keras.layers.Dense(28*28, activation=tf.nn.sigmoid, name='out'),
+  tf.keras.layers.Dense(50*50, activation=tf.nn.sigmoid, name='out'),
 ])
 
 model.compile(optimizer='adadelta',
               loss='binary_crossentropy',
               metrics=['accuracy'])
 
-model.fit(x_train, x_train, epochs=5)
+model.fit(train, train, epochs=50)
 
-model.evaluate(x_test, x_test)
+#model.evaluate(x_test, x_test)
 
 
 
 idx = 4  # index of desired layer
-layer_input = tf.keras.Input(shape=(50,)) # a new input tensor to be able to feed the desired layer
+layer_input = tf.keras.Input(shape=(100,)) # a new input tensor to be able to feed the desired layer
 
 # create the new nodes for each layer in the path
 x = layer_input
@@ -55,8 +57,8 @@ for layer in model.layers[idx:]:
 
 # create the model
 new_model = tf.keras.Model(layer_input, x)
-output = new_model.predict(np.full((1,50), 0.5))
-io.imshow((output[0] * 255.).reshape((28,28)).astype(int), cmap='gray')
+output = new_model.predict(np.full((1,100), 0.5))
+io.imshow((output[0] * 255.).reshape((50,50)).astype(int), cmap='gray')
 plt.show()
 
 new_model.save('model1.h5')
--- project_2/read_data.py
+++ project_2/read_data.py
@@ -1,21 +1,22 @@
-import re
-import urllib.request
+import os
+import cv2
+import numpy as np
 
-f_m = open("facescrub_actors.txt","r")
-f_w = open("facescrub_actresses.txt","r")
-i = 0
+def read_data(path): 
+    files = []
+    # r=root, d=directories, f = files
+    for r, _d, f in os.walk(path):
+        for file in f:
+            files.append(os.path.join(r, file))
 
-for l in f_m.readlines():
-    x = re.search("http[^\s]+", l)
-    print(l)
-    if x != None:
-        try:
-            url = x.group()
-            urllib.request.urlretrieve(url, "imgs/" + str(i) + ".jpg")
-            i += 1
-        except:
-            print("deu ruim")
-            pass
+    images = []
+    i = 0
+    for f in files:
+        image = cv2.imread(f)
+        gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+        rez_image = cv2.resize(gray,(50,50))
+        images.append(rez_image)
+        print(i)
+        i += 1
 
-f_m.close()
-f_w.close()
+    return np.array(images)
