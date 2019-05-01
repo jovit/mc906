e19888fe566b8c5d0ce596cf3985c10291628f85
João Vitor Gonçalves <joao_vitor_araki_goncalves@hotmail.com>
2019-05-01 17:05:19 -0300

    wip

--- project_2/main.py
+++ project_2/main.py
@@ -1,17 +1,20 @@
+from read_data import read_data
+from skimage import io
+from matplotlib import pyplot as plt
+import numpy as np
+import tensorflow as tf
 import os
 
-os.environ['KMP_DUPLICATE_LIB_OK']='True'
+os.environ['KMP_DUPLICATE_LIB_OK'] = 'True'
 
-import tensorflow as tf
-import numpy as np
-from matplotlib import pyplot as plt
-from skimage import io
-from read_data import read_data
 
 mnist = tf.keras.datasets.mnist
 
 train = read_data('imgs/train/').astype(float)
-train = train / 255
+train = train / 255.
+
+test = read_data('imgs/test/').astype(float)
+test = test / 255.
 
 train = np.array([it.flatten() for it in train])
 print(len(train[0]))
@@ -22,33 +25,41 @@ print(len(train[0]))
 # plt.show()
 
 
-
 model = tf.keras.models.Sequential([
-  tf.keras.layers.Dense(50*50),
+    tf.keras.layers.Dense(50*50),
 
-  tf.keras.layers.Dense(1000, activation=tf.nn.relu),
-  tf.keras.layers.Dropout(0.2),
+    tf.keras.layers.Dense(2000, activation=tf.nn.relu),
+    tf.keras.layers.Dropout(0.2),
+    tf.keras.layers.Dense(1500, activation=tf.nn.relu),
+    tf.keras.layers.Dropout(0.2),
+    tf.keras.layers.Dense(1000, activation=tf.nn.relu),
+    tf.keras.layers.Dropout(0.2),
 
-  tf.keras.layers.Dense(100,  activation=tf.nn.sigmoid),
 
-  tf.keras.layers.Dense(1000, activation=tf.nn.relu),
-  tf.keras.layers.Dropout(0.2),
+    tf.keras.layers.Dense(250,  activation=tf.nn.sigmoid),
 
-  tf.keras.layers.Dense(50*50, activation=tf.nn.sigmoid, name='out'),
+    tf.keras.layers.Dense(1000, activation=tf.nn.relu),
+    tf.keras.layers.Dropout(0.2),
+    tf.keras.layers.Dense(1500, activation=tf.nn.relu),
+    tf.keras.layers.Dropout(0.2),
+    tf.keras.layers.Dense(2000, activation=tf.nn.relu),
+    tf.keras.layers.Dropout(0.2),
+
+    tf.keras.layers.Dense(50*50, activation=tf.nn.sigmoid, name='out'),
 ])
 
 model.compile(optimizer='adam',
               loss='binary_crossentropy',
               metrics=['accuracy'])
 
-model.fit(train, train, epochs=100, batch_size=500)
+model.fit(train, train, epochs=20)
 
-#model.evaluate(x_test, x_test)
+model.evaluate(test, test)
 
 
-
-idx = 4  # index of desired layer
-layer_input = tf.keras.Input(shape=(100,)) # a new input tensor to be able to feed the desired layer
+idx = 8  # index of desired layer
+# a new input tensor to be able to feed the desired layer
+layer_input = tf.keras.Input(shape=(250,))
 
 # create the new nodes for each layer in the path
 x = layer_input
@@ -57,8 +68,8 @@ for layer in model.layers[idx:]:
 
 # create the model
 new_model = tf.keras.Model(layer_input, x)
-output = new_model.predict(np.full((1,100), 0.5))
-io.imshow((output[0] * 255.).reshape((50,50)).astype(int), cmap='gray')
+output = new_model.predict(np.full((1, 250), 0.5))
+io.imshow((output[0] * 255.).reshape((50, 50)).astype(int), cmap='gray')
 plt.show()
 
 new_model.save('model1.h5')
