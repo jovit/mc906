980bb0d45ef6049bdb358ce9dc8e1fd346393817
João Vitor Gonçalves <joao_vitor_araki_goncalves@hotmail.com>
2019-05-11 13:46:03 -0300

    different files for different problems

--- project_2/best_params_autoencoder_faces.py
+++ project_2/best_params_autoencoder_faces.py
@@ -10,13 +10,11 @@ from genetic_algorithm import Individual, Darwin, Population
 from read_data import read_data
 
 MIN_NEURONS = 10
-MAX_NEURONS = 500
+MAX_NEURONS = 2000
 MIN_LAYERS = 1
 MAX_LAYERS = 4
 MIN_FEAT = 20
-MAX_FEAT = 100
-MIN_EPOCHS = 1
-MAX_EPOCHS = 10
+MAX_FEAT = 1000
 
 
 def pairwise(iterable):
@@ -28,7 +26,7 @@ def pairwise(iterable):
 
 class Model(Individual):
     # [layers(0-4), neurons(100-2000), numfeatures(200, 1000), loss(binary_crossentropy, mse)]
-    def __init__(self, chromossome_mutation_rate, input_size, number_of_epochs, layers, neurons, num_features, train_set, test_set):
+    def __init__(self, chromossome_mutation_rate, input_size, layers, neurons, num_features, train_set, test_set):
         super().__init__(chromossome_mutation_rate)
         self.input_size = input_size
         self.layers = layers
@@ -37,40 +35,38 @@ class Model(Individual):
         self.train_set = train_set
         self.test_set = test_set
         self.loss = 0
-        self.accuracy = 0
         self.model = None
-        self.number_of_epochs = number_of_epochs
 
     def fitness(self):
+        tf.random.set_random_seed(1)
 
         if self.loss == 0:
             tf.keras.backend.clear_session()
             tf.random.set_random_seed(1)
 
-            layers = [tf.keras.layers.Flatten(input_shape=(28, 28))]
+            layers = [tf.keras.layers.Dense(self.input_size)]
             for i in range(self.layers // 2):
                 layers.append(tf.keras.layers.Dense(self.neurons[i], activation=tf.nn.relu))
-
+            layers.append(tf.keras.layers.Dense(self.num_features, activation=tf.nn.sigmoid))
+            for i in range(self.layers // 2, len(self.neurons)):
+                layers.append(tf.keras.layers.Dense(self.neurons[i], activation=tf.nn.relu))
+            
             layers.append(tf.keras.layers.Dense(self.input_size, activation=tf.nn.sigmoid))
 
             self.model = tf.keras.models.Sequential(layers)
 
-            self.model.compile(optimizer='adam', loss='sparse_categorical_crossentropy' ,metrics=['accuracy'])
+            self.model.compile(optimizer='adadelta', loss='binary_crossentropy')
 
-            self.model.fit(self.train_set[0], self.train_set[1], epochs=self.number_of_epochs, validation_data=(self.test_set[0], self.test_set[1]))
-            evaluation = self.model.evaluate(self.test_set[0], self.test_set[1])
-            self.loss = evaluation[0]
-            self.accuracy = evaluation[1]
-
+            self.model.fit(self.train_set, self.train_set, epochs=4, validation_data=(self.test_set, self.test_set))
+            self.loss = self.model.evaluate(self.test_set, self.test_set)
 
         return 1. / self.loss
 
     def clone(self):
         super().clone()
-        doppelganger = Model(self.chromossome_mutation_rate, self.input_size, self.number_of_epochs, self.layers, np.copy(self.neurons), self.num_features, self.train_set,
+        doppelganger = Model(self.chromossome_mutation_rate, self.input_size, self.layers, np.copy(self.neurons), self.num_features, self.train_set,
                              self.test_set)
         doppelganger.loss = self.loss
-        doppelganger.accuracy = self.accuracy
         return doppelganger
 
     def breed(self, other):
@@ -96,11 +92,8 @@ class Model(Individual):
         def __num_features_crossover(child):
             child.num_features = other.num_features
 
-        def __num_epochs_crossover(child):
-            child.number_of_epochs = other.number_of_epochs
-
         child = self.clone()
-        crossover_functions = [__neurons_crossover, __num_epochs_crossover]
+        crossover_functions = [__layers_crossover, __neurons_crossover, __num_features_crossover]
         crossover_features = random.randint(1, len(crossover_functions))
         for func in random.choices(crossover_functions, k=crossover_features):
             func(child)
@@ -125,11 +118,8 @@ class Model(Individual):
         def __mutate_num_features(other):
             other.num_features = np.random.randint(MIN_FEAT, MAX_FEAT)
 
-        def __mutate_num_epochs(other):
-            other.num_features = np.random.randint(MIN_EPOCHS, MAX_EPOCHS)
-
         other = self.clone()
-        mutate_functions = [__mutate_layers, __mutate_neurons, __mutate_num_epochs]
+        mutate_functions = [__mutate_layers, __mutate_neurons, __mutate_num_features]
         mutate_features = random.randint(1, len(mutate_functions))
         for func in random.choices(mutate_functions, k=mutate_features):
             func(other)
@@ -138,15 +128,14 @@ class Model(Individual):
         return other
 
     def __repr__(self):
-        return "Layers: {}, Neurons: {}, Loss: {}, Epochs: {}".format(self.layers, self.neurons, self.loss, self.number_of_epochs)
+        return "Layers: {}, Neurons: {}, # Features: {}, Loss: {}".format(self.layers, self.neurons, self.num_features, self.loss)
 
     @staticmethod
     def generate_random(chromossome_mutation_rate, train_set, test_set):
         layers = np.random.randint(MIN_LAYERS, MAX_LAYERS)
         neurons = np.random.random_integers(MIN_NEURONS, MAX_NEURONS, layers)
         num_features = np.random.randint(MIN_FEAT, MAX_FEAT)
-        num_epochs = np.random.randint(MIN_EPOCHS, MAX_EPOCHS)
-        return Model(chromossome_mutation_rate, 50 * 50, num_epochs, layers, neurons, num_features, train_set, test_set)
+        return Model(chromossome_mutation_rate, 50 * 50, layers, neurons, num_features, train_set, test_set)
 
 
 class FaceAutoencoderModels(Population):
@@ -189,32 +178,26 @@ if __name__ == '__main__':
     os.environ['KMP_DUPLICATE_LIB_OK'] = 'True'
     tf.random.set_random_seed(1)
 
-    mnist = tf.keras.datasets.mnist
+    # mnist = tf.keras.datasets.mnist
 
-    (x_train, y_train), (x_test, y_test) = mnist.load_data()
-    x_train, x_test = x_train / 255.0, x_test / 255.0
+    # (x_train, y_train), (x_test, y_test) = mnist.load_data()
+    # x_train, x_test = x_train / 255.0, x_test / 255.0
 
-    # train = read_data('imgs/train_aligned/').astype(float)
-    # train = train / 255.
+    train = read_data('imgs/train_aligned/').astype(float)
+    train = train / 255.
 
-    # test = read_data('imgs/test/').astype(float)
-    # test = test / 255.
+    test = read_data('imgs/test/').astype(float)
+    test = test / 255.
 
-    # train = np.array([it.flatten() for it in train])
-    # test = np.array([it.flatten() for it in test])
+    train = np.array([it.flatten() for it in train])
+    test = np.array([it.flatten() for it in test])
 
-    population = FaceAutoencoderModels.generate_random(size=10, chromossome_mutation_rate=0.1, mutation_rate=0.2, crossover_rate=0.6,
-                                                       train_set=(x_train, y_train), test_set=(x_test, y_test))
+    population = FaceAutoencoderModels.generate_random(size=5, chromossome_mutation_rate=0.1, mutation_rate=0.2, crossover_rate=0.6,
+                                                       train_set=train, test_set=test)
     result = Darwin.genetic_algorithm(population, 100)
     print("Best: {}".format(result[-1]))
     plot_data = list(map(lambda c: c.loss, result))
-    acc_data = list(map(lambda c: c.accuracy, result))
     plt.plot(plot_data)
     plt.ylabel('Loss')
-    plt.xlabel('Generation')
-    plt.show()
-
-    plt.plot(acc_data)
-    plt.ylabel('Accuracy')
     plt.xlabel('Generation')
     plt.show()
